91fd0d1eaa11f384fd52776b025fa7a6bec7fdbb
Jagaa <jagaa1103@gmail.com>
2016-02-16 23:22:58 +0100

    start Page init

---
+++ bairApp/app/pages/start/start.js
@@ -0,0 +1,11 @@
+import {Page} from 'ionic/ionic';
+
+@Page({
+    templateUrl: 'build/pages/start/start.html'
+})
+
+export class StartPage{
+    constructor(){
+        console.log('StartPage');
+    }
+}
